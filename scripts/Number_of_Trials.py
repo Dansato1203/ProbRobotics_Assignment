2cd4e1d641fe87a4c924f943cfe39987b0a1e48b
Dansato1203 <s19C1054bu@s.chibakoudai.jp>
2023-01-12 22:56:24 +0900

    Add '15 trial'

--- scripts/Number_of_Trials.py
+++ scripts/Number_of_Trials.py
@@ -42,6 +42,36 @@ def main():
 
 
 	# 試行回数を増やすと、一回試行を増やしただけでは完走率は変わらないのでは？
+	# 試行回数を15回に増やしてみる
+	print("\n")
+	print("試行回数 : 15回")
+
+	Before_Trial = ["完走", "失敗", "失敗", "完走", "完走"]
+	After_Trial = ["完走", "完走", "完走", "完走", "完走"]
+
+	# ここでは単純に5回の試行を20倍して100回の試行とする
+	Before_Trial_15 = Before_Trial * 3
+	After_Trial_15 = After_Trial * 3
+
+	# 改良前
+	t = Calc_FinishRate(Before_Trial_15)
+	print(f"改良前の完走率 : {t}")
+
+	# 改良後
+	t = Calc_FinishRate(After_Trial_15)
+	print(f"改良後の完走率 : {t}")
+
+	# 同様に改良前に"完走"を、改良後に"失敗"の試行を追加して完走率を計算してみる
+	# 改良前
+	Before_Trial_15.append("完走")
+	t = Calc_FinishRate(Before_Trial_15)
+	print(f"改良前の完走率(完走の試行を追加) : {t}")
+
+	# 改良後
+	After_Trial_15.append("失敗")
+	t = Calc_FinishRate(After_Trial_15)
+	print(f"改良後の完走率(失敗の試行を追加) : {t}")
+
 	# 試行回数を100回に増やしてみる
 	print("\n")
 	print("試行回数 : 100回")
